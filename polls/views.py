0a3c14bb189d918cdaba12a2f72995f19cbc87d1
Shahed <shahed_hridoy@outlook.com>
2022-10-09 23:17:13 +0600

    update

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.http import Http404
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -21,4 +21,8 @@ def results(reuest, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError Choice.DoesNotExist):
+    else:
